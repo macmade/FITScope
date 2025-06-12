a916e4a82673fcfc0062d5b62e35572d26d54dd0
macmade <macmade@xs-labs.com>
2025-06-12 10:14:46 +0200

    feat: Stretch control

--- FITScope/Views/ImageControls/StretchControlView.swift
+++ FITScope/Views/ImageControls/StretchControlView.swift
@@ -27,14 +27,103 @@ import SwiftUI
 
 public struct StretchControlView: View
 {
+    public enum Mode: CaseIterable, CustomStringConvertible
+    {
+        case none
+        case log
+        case arcsinh
+        case sigmood
+
+        public var description: String
+        {
+            switch self
+            {
+                case .none:    return "None"
+                case .log:     return "Logarithmic"
+                case .arcsinh: return "Inverse Hyperbolic Sine"
+                case .sigmood: return "Sigmoid"
+            }
+        }
+    }
+
+    @State private var mode               = Mode.none
+    @State private var showLogSliders     = false
+    @State private var showArcSinHSliders = false
+    @State private var showSigmoidSliders = false
+    @State private var logN1              = 0.0
+    @State private var arcsinhN1          = 0.0
+    @State private var sigmoidN1          = 0.0
+    @State private var sigmoidN2          = 0.0
+
     public var body: some View
     {
-        Text( "..." )
+        Grid( alignment: .leading )
+        {
+            GridRow
+            {
+                Text( "Mode" )
+                Picker( "Mode", selection: $mode )
+                {
+                    ForEach( Mode.allCases, id: \.self )
+                    {
+                        Text( $0.description ).tag( $0 )
+                    }
+                }
+                .labelsHidden()
+            }
+
+            if self.showLogSliders
+            {
+                SliderGridRowView( value: $logN1, minimumValue: 0, maximumValue: 255, label: "Intensity", image: "n.circle.fill" )
+            }
+
+            if self.showArcSinHSliders
+            {
+                SliderGridRowView( value: $arcsinhN1, minimumValue: 0, maximumValue: 255, label: "Factor", image: "n.circle.fill" )
+            }
+
+            if self.showSigmoidSliders
+            {
+                SliderGridRowView( value: $sigmoidN1, minimumValue: 0, maximumValue: 255, label: "Midpoint", image: "n.circle.fill" )
+                SliderGridRowView( value: $sigmoidN2, minimumValue: 0, maximumValue: 255, label: "Contrast", image: "n.circle.fill" )
+            }
+        }
+        .onChange( of: self.mode )
+        {
+            switch mode
+            {
+                case .none:
+
+                    self.showLogSliders     = false
+                    self.showArcSinHSliders = false
+                    self.showSigmoidSliders = false
+
+                case .log:
+
+                    self.showLogSliders     = true
+                    self.showArcSinHSliders = false
+                    self.showSigmoidSliders = false
+
+                case .arcsinh:
+
+                    self.showLogSliders     = false
+                    self.showArcSinHSliders = true
+                    self.showSigmoidSliders = false
+
+                case .sigmood:
+
+                    self.showLogSliders     = false
+                    self.showArcSinHSliders = false
+                    self.showSigmoidSliders = true
+            }
+        }
     }
 }
 
 #Preview
 {
     StretchControlView()
+        .frame( maxWidth: .infinity, alignment: .leading )
+        .frame( maxHeight: .infinity, alignment: .top )
         .padding()
 }
